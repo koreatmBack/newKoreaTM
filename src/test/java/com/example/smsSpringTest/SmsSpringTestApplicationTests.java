0c6a8d23d7048eb0eb65bd263d06c512a6cd975a
skh9805 <skh9805@naver.com>
2024-10-07 10:05:13 +0900

    new commit

--- src/test/java/com/example/smsSpringTest/SmsSpringTestApplicationTests.java
+++ src/test/java/com/example/smsSpringTest/SmsSpringTestApplicationTests.java
@@ -3,11 +3,16 @@ package com.example.smsSpringTest;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.Assert.assertEquals;
+
 @SpringBootTest
 class SmsSpringTestApplicationTests {
 
 	@Test
 	void contextLoads() {
+		int a=1;
+		int b=1;
+		assertEquals(a,b);
 	}
 
 }
