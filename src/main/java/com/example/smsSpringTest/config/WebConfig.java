51df05c0df337e7ea1d8f5411f0008ad3e180476
skh9805 <skh9805@naver.com>
2024-10-15 14:38:37 +0900

    cors에 "OPTIONS" 제거

--- src/main/java/com/example/smsSpringTest/config/WebConfig.java
+++ src/main/java/com/example/smsSpringTest/config/WebConfig.java
@@ -17,7 +17,7 @@ public class WebConfig implements WebMvcConfigurer {
 public void addCorsMappings(CorsRegistry registry) {
     registry.addMapping("/**") // 모든 경로에 대해 CORS 허용
             .allowedOrigins("http://localhost:3013", "http://192.168.0.34:3013","http://localhost:3014", "http://192.168.0.34:3014", "http://koti-jobsite.s3-website.ap-northeast-2.amazonaws.com" ) // 허용할 도메인
-            .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // 허용할 HTTP 메서드
+            .allowedMethods("GET", "POST", "PUT", "DELETE") // 허용할 HTTP 메서드
             .allowedHeaders("*") // 허용할 헤더
             .allowCredentials(true) // 자격 증명 허용 (예: 쿠키)
             .maxAge(3600); // preflight 요청을 캐싱하는 시간 (초 단위)
