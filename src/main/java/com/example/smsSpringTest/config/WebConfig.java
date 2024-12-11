9cb52dcd9e558494ee4a67b5f784fb17fbed0256
skh9805 <skh9805@naver.com>
2024-12-11 18:07:31 +0900

    webconfig 모든 도메인 허용

--- src/main/java/com/example/smsSpringTest/config/WebConfig.java
+++ src/main/java/com/example/smsSpringTest/config/WebConfig.java
@@ -25,7 +25,7 @@ public void addCorsMappings(CorsRegistry registry) {
             .allowedOrigins("*")
             .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // 허용할 HTTP 메서드
             .allowedHeaders("*") // 허용할 헤더
-            .allowCredentials(true) // 자격 증명 허용 (예: 쿠키)
+//            .allowCredentials(true) // 자격 증명 허용 (예: 쿠키)
             .maxAge(3600); // preflight 요청을 캐싱하는 시간 (초 단위)
 }
 }
