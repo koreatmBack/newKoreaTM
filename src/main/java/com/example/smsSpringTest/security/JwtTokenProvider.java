2a2883b7ac11520f4b934286f8a54acf231fceab
skh9805 <skh9805@naver.com>
2025-01-21 10:18:47 +0900

    쿠키 다시 1시간

--- src/main/java/com/example/smsSpringTest/security/JwtTokenProvider.java
+++ src/main/java/com/example/smsSpringTest/security/JwtTokenProvider.java
@@ -40,8 +40,8 @@ public class JwtTokenProvider {
     private static final String BEARER_TYPE = "bearer";
     private static final String TYPE_ACCESS = "access";
     private static final String TYPE_REFRESH = "refresh";
-//    private static final long ACCESS_TOKEN_EXPIRE_TIME = 1000 * 60 * 60;  // 1시간
-    private static final long ACCESS_TOKEN_EXPIRE_TIME =  360 * 1000 ;  // 6분
+    private static final long ACCESS_TOKEN_EXPIRE_TIME = 1000 * 60 * 60;  // 1시간
+//    private static final long ACCESS_TOKEN_EXPIRE_TIME =  360 * 1000 ;  // 6분
 //    private static final long ACCESS_TOKEN_EXPIRE_TIME = 1000L * 60 * 60 * 24 * 30;    // api 작업용 한달
 
     private static final long REFRESH_TOKEN_EXPIRE_TIME = 1000L * 60 * 60 * 24 * 30;  // 30일
